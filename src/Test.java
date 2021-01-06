e28147c53b9056f019059c6ba97c49a3bf260f5b
24012401 <2697083380@qq.com>
2021-01-06 15:57:07 +0800

    新建FX项目

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("lxx test");
-        System.out.println("syx test");
-        System.out.println("syx second test");
-        System.out.println("zhw test");
-        System.out.println("fhy test");
-    }
-}
